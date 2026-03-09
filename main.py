f8063b8fef29e680f7257b3a4dafd5a5cfa6a623
mumtazgj <j.mumtaz-garcia1@lancaster.ac.uk>
2026-03-09 13:16:06 +0000

    Created class and function structure

---
+++ main.py
@@ -0,0 +1,19 @@
+class NLPClassification:
+    def __init__(self):
+        pass
+    def dataCleaning(df):
+        pass
+    def featureEmbedding(df):
+        pass
+    def featureEmbedding2(df):
+        pass
+    def buildModel(df):
+        pass
+    def classification(df):
+        pass
+    def evaluation(df):
+        pass
+
+if __name__ == "__main__":
+    #Initialise the classification class
+    classifier = NLPClassification()
