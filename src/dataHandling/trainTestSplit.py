2826b46683d398f73807f2015c7d02ec05f69760
mumtazgj <j.mumtaz-garcia1@lancaster.ac.uk>
2026-03-14 21:38:27 +0000

    created the test train split function for the dataset

--- src/dataHandling/trainTestSplit.py
+++ src/dataHandling/trainTestSplit.py
@@ -1,2 +1,9 @@
+from sklearn.model_selection import train_test_split
+
 def trainTestSplit(df):
-    return
+    X = df["transcript"]
+    y = df["party"]
+    
+    trainX, trainY, testX, testY = train_test_split(X, y, test_size=0.2, random_state=123)
+
+    return trainX, trainY, testX, testY
