182572aa4ef4605e9dfde43a041870241738fa0c
mumtazgj <j.mumtaz-garcia1@lancaster.ac.uk>
2026-03-16 16:23:59 +0000

    moved these functions into different files so theres not too many files

--- src/dataHandling/trainTestSplit.py
+++
@@ -1,9 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-def trainTestSplit(df):
-    X = df["transcript"]
-    y = df["party"]
-    
-    trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.2, random_state=123)
-
-    return trainX, trainY, testX, testY
