1de578bc0821d3a1069ed187802c976535d77b48
mumtazgj <j.mumtaz-garcia1@lancaster.ac.uk>
2026-03-16 16:23:02 +0000

    implemented svm and random forest models

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,4 @@
-from sklearn.model_selection import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -9,10 +9,6 @@ def logRegModel(X, y):
     return model
 
 def svmModel(X,y):
-    #scale x for the model
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-
     #fit the svc model
     model = LinearSVC().fit(X, y)
     return model
