b42fdb97bd0b70dc25ec018e9eb82e786ea873b6
mumtazgj <j.mumtaz-garcia1@lancaster.ac.uk>
2026-03-16 16:22:34 +0000

    accuracy, classification report and confusion matrix evaluation functions

--- src/evaluation/evaluate.py
+++ src/evaluation/evaluate.py
@@ -1,11 +1,19 @@
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
-def evaluate(model, X, y):
+def accuracy(model, X, y):
     #make predictions on the test X values
     yPredictions = model.predict(X)
 
     #calculate the accuracy from the true values of y and the predicitons made by the model
     accuracy = accuracy_score(y, yPredictions)
+    return accuracy
 
+def classReport(model, X, y):
+    yPredictions = model.predict(X)
+    print(classification_report(y, yPredictions))
+
+def confusionMatrix(model, X, y):
+    #make predictions on the test X values
+    yPredictions = model.predict(X)
     #create a confusion matrix
-    print(accuracy)
+    print(confusion_matrix(y, yPredictions))
