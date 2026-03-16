4b4440f97672511b7c51c7d4505d896aeea434ba
mumtazgj <j.mumtaz-garcia1@lancaster.ac.uk>
2026-03-16 16:23:29 +0000

    calling all functions defined in order, need to tidy up once finished

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from src.dataHandling.loadData import loadData
-from src.dataHandling.dataPreprocessing import preprocess
-from src.dataHandling.trainTestSplit import trainTestSplit
-from src.embeddings.tfIDF import TFIDF
+from src.dataHandling.dataPreprocessing import preprocess, chunkify, trainTestSplit
+from src.embeddings.embeddings import TFIDF, word2vec, sentenceTransform
 from src.models.models import logRegModel, svmModel, rfModel
-from src.evaluation.evaluate import evaluate
+from src.evaluation.evaluate import accuracy, confusionMatrix, classReport
 
 #load the raw data from the data folder
 df = loadData()
@@ -12,17 +11,50 @@ df = loadData()
 df["transcript"] = df["transcript"].apply(preprocess)
 
 #split the data into train and test
-trainX, trainY, testX, testY = trainTestSplit(df)
+dfTrain, dfTest = trainTestSplit(df)
 
+#split the data into chunks
+trainChunks = chunkify(dfTrain,100)
+trainX = [chunk[0] for chunk in trainChunks]
+trainY = [chunk[1] for chunk in trainChunks]
+
+testChunks = chunkify(dfTest,100)
+testX = [chunk[0] for chunk in testChunks]
+testY = [chunk[1] for chunk in testChunks]
+
+print(len(trainX))
+print(len(testX))
+
+'''
 #embed the data with each method
-embeddedTrain, embeddedTest = TFIDF(trainX, testX)
+tfidfTrain, tfidfTest = TFIDF(trainX, testX)
+word2vecTrain, word2vecTest = word2vec(trainX, testX)
+#sentTransTrain, sentTransTest = sentenceTransform(trainX, testX)
 
 #train each model with each embedding
-LRmodel = logRegModel()
-SVMmodel = svmModel()
-RandomForestModel = rfModel()
+logistic_regression_model2 = logRegModel(tfidfTrain, trainY)
+simple_vector_machine_model2 = svmModel(tfidfTrain, trainY)
+random_forest_model2 = rfModel(tfidfTrain, trainY)
+logistic_regression_model = logRegModel(word2vecTrain, trainY)
+simple_vector_machine_model = svmModel(word2vecTrain, trainY)
+random_forest_model = rfModel(word2vecTrain, trainY)
 
 #evaluate each model
-evaluate(LRmodel, embeddedTest, testY)
-evaluate(SVMmodel, embeddedTest, testY)
-evaluate(RandomForestModel, embeddedTest, testY)
+lrAcc2 = accuracy(logistic_regression_model2, tfidfTest, testY)
+svmAcc2 = accuracy(simple_vector_machine_model2, tfidfTest, testY)
+rfAcc2 = accuracy(random_forest_model2, tfidfTest, testY)
+print("="*50)
+print("TF-IDF")
+print(f"Logistic Regression: {lrAcc2:.3f}")
+print(f"Simple Vector Machine: {svmAcc2:.3f}")
+print(f"Random Forest: {rfAcc2:.3f}")
+print("="*50)
+lrAcc = accuracy(logistic_regression_model, word2vecTest, testY)
+svmAcc = accuracy(simple_vector_machine_model, word2vecTest, testY)
+rfAcc = accuracy(random_forest_model, word2vecTest, testY)
+print("="*50)
+print("Word2Vec")
+print(f"Logistic Regression: {lrAcc:.3f}")
+print(f"Simple Vector Machine: {svmAcc:.3f}")
+print(f"Random Forest: {rfAcc:.3f}")
+print("="*50)'''
